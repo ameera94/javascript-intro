927980f9f1bdaef7fd25c9512bcf2f9e43a48af0
ameera94 <ashah2@spartaglobal.com>
2019-01-11 11:44:10 +0000

    initial commit

---
+++ javascript-intro/js/main.js
@@ -0,0 +1,30 @@
+// strings should be in ""
+
+// Alert .. can only close when click ok
+alert("Hello!");
+
+// communicate in console
+console.log("Hi in console!");
+
+// to assign a variable give it a name and "="  (var name = value)
+var myNumber = 90;
+var yourNumber = 70;
+
+// will calculate in console
+console.log(myNumber * yourNumber)
+
+sum = myNumber + yourNumber
+
+console.log("Number", myNumber, "plus", yourNumber, "equals", sum);
+
+// Boolean expression - will only show value that will be true
+var myNumber = true;
+var yourNumber = 70;
+
+console.log("Number", myNumber, "plus", yourNumber, "equals", sum);
+
+// Can assign multiple values to variable using []. Selecting value --- order = [0, 1 , 2, 3 , 4, etc.]
+var myNumber2 = [100, 6, 90]
+
+console.log(myNumber2[0] + myNumber2[2]);
+
